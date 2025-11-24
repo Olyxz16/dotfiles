6ca203e1449354142c203bef0536674f486b551d
Olyxz16 <cedric.sazos@tutanota.com>
2025-11-24 20:19:08 +0100

    change trouble keybind

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -5,7 +5,7 @@ return {
     cmd = "Trouble",
     keys = { 
         {
-            "<leader>tt",
+            "<leader>tr",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
