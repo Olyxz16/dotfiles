44c4cd237941a0af02f2eb04015ac04d19389dd3
Olyxz16 <cedric.sazos@tutanota.com>
2026-01-19 14:08:15 +0100

    fix sway gnome conflict

--- sway/scripts/color-scheme-toggle.sh
+++ sway/scripts/color-scheme-toggle.sh
@@ -4,8 +4,6 @@ current_scheme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ $current_scheme == *"dark"* ]] || [[ $current_scheme == *"Dark"* ]]; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-fi
-
-if [[ $current_scheme == *"light"* ]] || [[ $current_scheme == *"Light"* ]]; then
+else 
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 fi
